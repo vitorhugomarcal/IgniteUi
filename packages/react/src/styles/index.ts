5d049612e5ad9a12bc7eb99481224a7149db4c92
vitorhugomarcal <vitorhugomarcal@gmail.com>
2022-10-21 22:06:34 -0300

    fix: @ignite-ui to @vhmarcal-ui

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@vhamrcal-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
